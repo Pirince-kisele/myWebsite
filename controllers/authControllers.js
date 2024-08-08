bfb8da168fbc2bac212bf6b0475228b23713c376
pirince <pliyo@ivytech.edu>
2024-08-08 03:07:13 -0500

    update1

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../model/user");
 const jwt = require('jsonwebtoken');
 
 // handle errors
--- index.js
+++ index.js
@@ -1,53 +1,37 @@
 const express = require('express');
 const { env, title } = require('process');
-const app = express();
+const layout = require("express-layout")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const blogsChema = require("./model/blog")
 const fs = require("fs")
 const path = require("path")
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
+
+const app = express();
 
 
 // import route
 const authRoutes = require("./routes/auth.js")
 const blogRoutes = require("./routes/blogPost.js")
-
+// template engine
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 require("dotenv").config();
+// body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static.public);
 app.use(express.json());
 
-//this code is for upload the image in the db
-var multer = require("multer");
-var storage = multer.diskStorage({
-   destination:(req, file, cb) =>{
-      cb(null,file.fieldname + "-" + Date.now())
-   }
-});
-var upload = multer({storage: storage})
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-
-
-app.post("/", upload.single("image"),(req, res, next) =>{
-var obj = {
-   title: req.body.title,
-   content: req.body.content,
-   image: {
-      data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.fieldname)),
-      contentType: "image/png"
-   },
-   likes: req.body.likes,
-   comments: req.body.comments
-}
-const blog = blogsChema.create(obj)
-
+app.listen(PORT, ()=>{
+   console.log(`serveer is listining in Port ${PORT}`)
 })
 
-app.use("api/auth", authRoutes)
-app.use("api/blogs",blogRoutes)
+
+
+
