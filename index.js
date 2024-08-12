b093bbae8e59c64b27ba6cf7fb2cdc14640d4715
pirince <pliyo@ivytech.edu>
2024-08-11 22:46:57 -0500

    update

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const express = require('express');
-const { env, title } = require('process');
-const layout = require("express-layout")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const blogsChema = require("./model/blog")
-const fs = require("fs")
-const path = require("path")
-const port = process.env.PORT || 5000;
+const express = require("express");
+const layots = require("express-layout");
+const mongoose = require("mongoose");
+const Blog = require("./model/blog")
+
+
+
 
 const app = express();
 
-// body parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(express.static.public);
-app.use(express.json());
-// import route
-const authRoutes = require("./routes/auth.js")
-const blogRoutes = require("./routes/blogPost.js")
-// template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-require("dotenv").config();
 
+const port = 3000;
 
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, ()=>{
-   console.log(`serveer is listining in Port ${port}`)
+   console.log(`the server is listining to the port ${port}`)
 })
-
-
-
-
